b76894230cf9ccef1a89623d6742cbbce7c0346a
hophacker <jiefeng.hopkins@gmail.com>
2016-04-07 13:52:25 +0800

    enable loader for css, split webpack configs into dev and production modes

--- app/index.js
+++ app/index.js
@@ -1,3 +1,7 @@
 var component = require('./component');
+var app = document.createElement('div');
+require('./index.css');
 
-document.body.appendChild(component());
+document.body.appendChild(app);
+
+app.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const TARGET = process.env.npm_lifecycle_event;
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 }
 
-
-module.exports = {
+var config = {
   entry: {
     app: PATHS.app
   },
@@ -20,5 +20,47 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Webpack demo'
     })
-  ]
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+        include: PATHS.app
+      }
+    ]
+  },
 }
+
+if(TARGET === 'start') {
+  Object.assign(config, {
+    devServer: {
+      contentBase: PATHS.build,
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+      // Parse host and port from env so this is easy to customize.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices unlike default
+      // localhost
+      host: process.env.HOST || 'localhost',
+      port: process.env.PORT || 8080
+    }
+  })
+  config.plugins = config.plugins.concat([
+    new webpack.HotModuleReplacementPlugin()
+  ])
+}
+
+if(TARGET === 'build') {
+}
+module.exports = config 
